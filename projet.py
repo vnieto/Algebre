369e5096dabc90ed6e2758ef5369bf4abb3ea9ac
Nieto Vincent <vnieto@bs406-s16-04.insa-lyon.fr>
2015-01-19 09:42:16 +0100

    importation of the 2, 3 columns from the BIOGRID file

--- projet.py
+++ projet.py
@@ -1,2 +1,8 @@
+import numpy as numpy
+t = numpy.genfromtxt('BIOGRID-ORGANISM-Rattus_norvegicus-3.2.120.tab2.txt',skip_header=1,usecols = (1, 2))
+N = len(t) # importation of the 2, 3 columns from the BIOGRID file!
+print "N =",N
+a = zeros(N,N)
 
-zeros(4977,4977)
+# Matrice adj
+# commande: unique
